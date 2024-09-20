5a3dfa2c020132ff11e9c595ab884ba878b71a4a
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2024-09-20 19:04:11 -0300

    crate new project

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# beautiful-garden
+# Beautiful Garden Shop
+
+
+### run project tailwind
+
+```sh
+npm run watch
+
+```
+
+and  _open with live server_ or "Alt+L Alt+O"
+
+## Tailwind Installation
+[TAILWINDCSS](https://tailwindcss.com/docs/installation)
+
+
+[PNGWING](https://www.pngwing.com/pt/search?q=png+gr%C3%A1tis)
+
+[PNGEGG](https://www.pngegg.com/pt)
+
+[PNGTREE](https://pt.pngtree.com/)
+
+
+|author: | IvanC_Code| :heart: |
+|--------|-----------|------------|
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px",
+    },
+    extend: {},
+    fontFamily: {
+      Jost: ["Jost", "sans-serif"],
+      Lobster: ["Lobster", "sans-serif"],
+      Concert: ["Concert One", "system-ui"],
+    },
+    container: {
+      center: true, 
+      padding: {
+        DEFAULT: "12px",
+        md: "32px",
+      }
+    }
+  },
+  plugins: [],
+}
