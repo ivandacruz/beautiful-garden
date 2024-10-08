5e8668af2deb3f7a7d06184cdf9fd1d99db7bc04
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2024-10-07 22:35:24 -0300

    add imgs flowers

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,25 @@ module.exports = {
       xl: "1180px",
     },
     extend: {},
+    keyframes: {
+      move:{
+        "50%":{transform: "translateY(-1rem)"}
+      },
+      rotate: {
+        "0%": { transform: "rotate(0deg" },
+        "100%": { transform: "rotate(360deg" },
+      },
+      scaleUp: {
+        "0%" : { transform: "scale(0.8)" },
+        "50%" : { transform: "scale(1.2)" },
+        "100%" : { transform: "scale(0.8)" },
+      },
+    },
+    animation: {
+      movingY: "move 3s linear infinite",
+      rotateng: "rotate 15s linear infinite",
+      scaleUp: "scaleUp 3s linear infinite", 
+    },
     fontFamily: {
       Jost: ["Jost", "sans-serif"],
       Lobster: ["Lobster", "sans-serif"],
