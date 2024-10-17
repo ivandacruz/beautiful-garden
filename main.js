415f80d692785b065ce10d3729caff3bf9501e02
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2024-10-16 22:09:53 -0300

    add footer

--- main.js
+++ main.js
@@ -24,6 +24,33 @@ navLink.forEach(link => {
 // Change background header
 
 // swipper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    },
+});
 
 // scroll sections active link
 
