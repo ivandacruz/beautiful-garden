cc5471351bd1e126948502aba94d0dbf503e47d7
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2024-10-24 18:43:16 -0300

    create deploy

--- main.js
+++ main.js
@@ -115,10 +115,12 @@ const sr = ScrollReveal({
     reset: true
 })
 
-sr.reveal(`.home__data, .about__top, .popular__top`)
+sr.reveal(`.home__data, .about__top, .popular__top, .review__top, .review__swiper, .footer__icon, .footer__content, .footer__floral, .copy__right`)
 sr.reveal(`.home__image`, {delay: 500, scale: 0.5})
-sr.reveal(`.service__card`, {delay: 500, scale: 0.5}, {interval: 100})
+sr.reveal(`.service__card, .popular__card, .footer__newsletter`, {delay: 500, scale: 0.5}, {interval: 100})
 
 sr.reveal(`.about__leaf`, {delay: 1000, origin: "right"} )
 sr.reveal(`.about__item__1-content, .about__item__2-img`, {origin: "right"} )
 sr.reveal(`.about__item__1-img, .about__item__2-content`, {origin: "left"} )
+
+sr.reveal(`.review__leaf, .footer__floral`, {delay: 1000, origin: "left"} )
